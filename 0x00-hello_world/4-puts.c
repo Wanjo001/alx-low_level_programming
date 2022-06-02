304ec8d4e37ee627699b15bc18be9daff5e1487f
wanjo001 <victorwanjohi6103009@gmail.com>
2022-06-02 07:47:22 -0700

    C_4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - print string in the puts function
+ * main - print string in the Puts function
  * Description: prints "programming is like building a multilingual puzzle"
  * Return: 0
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle")
+Puts("\"programming is like building a multilingual puzzle")
 return (0);
 }
