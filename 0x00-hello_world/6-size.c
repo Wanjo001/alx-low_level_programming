591f33e14ce2cf733120c90b6dc495df47a74c32
wanjo001 <victorwanjohi6103009@gmail.com>
2022-06-02 16:01:55 -0700

    C-6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
-
 /**
- *main - return integal of 0
+ * main - return integral of 0
  *
- *Description: print the size of diffirent characters and words
+ * Description:print the size of different characters and words
  *
- *Return: Always succes return o
+ * Return:Always success return 0
  *
  */
-
 int main(void)
 {
-printf("Size of a char: %ld byte(s)\n", sizeof(char));
-printf("Size of an int: %ld byte(s)\n", sizeof(int));
-printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 }
