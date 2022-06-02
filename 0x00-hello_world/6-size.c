5e3555a02cded710e33b1b00466d96d0ce293dd6
wanjo001 <victorwanjohi6103009@gmail.com>
2022-06-02 16:16:41 -0700

    C_6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  *
  * Description: print the size of diffirent characters and words
  *
- * Return: Always succes return 0
+ * Return: Always succes return o
  *
  */
 
@@ -16,4 +16,6 @@ int main(void)
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
 }
+
